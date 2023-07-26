f9100ab8dfa6da11d1f48bbbf5a73bb2233bfcf5
minh-truss <minh@truss.works>
2023-07-26 17:36:03 -0400

    add CI for matomo deploy

---
+++ scripts/deploy-dev-aws.sh
@@ -0,0 +1,25 @@
+set -e
+
+IMAGE=${ECR_REGISTRY}/${ECR_REPOSITORY}:${IMAGE_TAG}
+TASK_FAMILY="Matomo"
+
+TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition keystone --query '{
+                containerDefinitions: taskDefinition.containerDefinitions,
+                family: taskDefinition.family,
+                taskRoleArn: taskDefinition.taskRoleArn,
+                executionRoleArn: taskDefinition.executionRoleArn,
+                networkMode: taskDefinition.networkMode,
+                volumes: taskDefinition.volumes,
+                placementConstraints: taskDefinition.placementConstraints,
+                requiresCompatibilities: taskDefinition.requiresCompatibilities,
+                cpu: taskDefinition.cpu,
+                memory: taskDefinition.memory
+            }'> tmp-td.json)
+
+NEW_TASK_DEFINTION=$(jq -r --arg NEWIMAGE "$IMAGE" '.containerDefinitions[].image |= $NEWIMAGE' tmp-td.json > tmp-ntd.json)
+NEW_TASK_INFO=$(aws ecs register-task-definition --cli-input-json file://tmp-ntd.json)
+NEW_REVISION=$(echo $NEW_TASK_INFO | jq '.taskDefinition.revision')
+aws ecs update-service --cluster app-portal-client-dev \
+                        --service Matomo \
+                        --task-definition ${TASK_FAMILY}:${NEW_REVISION} \
+                        --force-new-deployment
